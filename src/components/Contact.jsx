ff2daedde62e3c16592640c9e47faeef9d2f238b
Saurabh Nishad <saurabhnishad1008@gmail.com>
2026-09-08 21:48:25 +0530

    mail in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,8 @@ function Contact() {
                 <h2>Looking for a Java Full Stack opportunity</h2>
                 <p>Feel free to reach out for opportunities, collaborations, or a conversation about software development.</p>
                 <div className="contact-links">
-                    <a href="mailto:nsaurabh42050@gmail.com"><Mail /> nsaurabh42050@gmail.com</a>
+                    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=nsaurabh42050@gmail.com"
+                    target="_blank"><Mail /> nsaurabh42050@gmail.com</a>
                     <a href="tel:+917977219034"><Phone /> +91 7977219034</a>
                     <a href="https://github.com/saurabh-alt" target="_blank" rel="noreferrer"><GithubIcon /> github.com/saurabh-alt</a>
                     <a href="https://www.linkedin.com/in/saurabh-nishad-872b25249/" target="_blank" rel="noreferrer"><Linkedin /> LinkedIn</a>
